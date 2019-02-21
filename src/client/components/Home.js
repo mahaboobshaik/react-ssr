03a5f26f0d72753df75c67f4b9a3db5777a84675
Mahaboobbasha Shaik <mahaboobbashashaik@Mahaboobbashas-MacBook-Pro-4.local>
2019-02-21 17:48:58 +0530

    Merging webpack and renderer helper (refactoring)

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <div>I'm the home component</div>
+            <div>I'm the home component new</div>
             <button onClick={() => { console.log('Hi there!')}}>Press Me!</button>
         </div>
         

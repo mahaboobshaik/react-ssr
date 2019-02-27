7c6feefc73bdb5b873b3c8f5cc8971ffc1036c1e
Mahaboobbasha Shaik <mahaboobbashashaik@Mahaboobbashas-MacBook-Pro-4.local>
2019-02-27 15:08:58 +0530

    added client and server side redux operations and data loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -7,11 +9,19 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
+    
     const store = createStore();
 
     // Logic to initialize and load data into the store
+    // console.log(matchRoutes(Routes, req.path));
+    const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+        return route.loadData ? route.loadData(store) : null;
+    });
 
-    res.send(renderer(req, store));
+    Promise.all(promises).then(() => {
+        res.send(renderer(req, store));
+    });
+
 });
 
 app.listen(3000, () => {
